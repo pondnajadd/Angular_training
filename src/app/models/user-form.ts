dec705491a67be45d6e24037220703b7ec95635d
thanakrit rachapong <thanakritr.p@gmail.com>
2024-03-19 12:08:04 +0700

    auth test

---
+++ src/app/models/user-form.ts
@@ -0,0 +1,10 @@
+export class UserForm {
+  username: string;
+  email: string;
+  password: string;
+  constructor() {
+    this.username = '';
+    this.password = '';
+    this.email = '';
+  }
+}
